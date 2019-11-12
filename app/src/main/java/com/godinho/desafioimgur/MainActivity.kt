299a1877c98012e9575b2613b15ce064a493456a
Rafael <rafaelgodinhobrandao@gmail.com>
2019-11-11 21:20:42 -0300

    - Add main activity

---
+++ app/src/main/java/com/godinho/desafioimgur/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.godinho.desafioimgur
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
